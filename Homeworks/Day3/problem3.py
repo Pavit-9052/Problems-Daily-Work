d508dfb957c431ad51672695ea3990cb72f9e06b
Pavithra M <134091742+Pavit-9052@users.noreply.github.com>
2023-11-29 19:02:45 +0530

    Create problem3.py

---
+++ Homeworks/Day3/problem3.py
@@ -0,0 +1,6 @@
+'''
+Problem #3
+From the same file as above, read from the file, count the number of unique 4 letter words and it no of occurrences
+for each word. Write this information at the end of file under the heading "Summary of 4 letter words"
+Make sure your code includes exception handling for reading from a file.
+'''
