c4ee0cc49eb40e7e50b50cb44bef67311d9f1523
Pavithra M <134091742+Pavit-9052@users.noreply.github.com>
2023-11-29 19:03:06 +0530

    Update problem2.py

--- Homeworks/Day3/problem2.py
+++ Homeworks/Day3/problem2.py
@@ -2,10 +2,9 @@
 Problem #2
 Read a passage from a file. 
 Count the number of times the word 'the' followed by another 'the' without 'a' in between.
-
 Eg The king went to the forest with the wife and a servernt. The king shot a deer. The king went to the forest again the next day.
-
 answer is 4 (The king, the forest, The King the next).
+Make sure your code includes exception handling for reading from a file.
 '''
 
 
