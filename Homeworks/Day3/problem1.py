255f9a92a24948315a6525f70dfbfaa9b08fee3c
Pavithra M <134091742+Pavit-9052@users.noreply.github.com>
2023-11-29 18:49:34 +0530

    Create problem1.py

---
+++ Homeworks/Day3/problem1.py
@@ -0,0 +1,19 @@
+'''
+Problem 1
+Print the following pattern
+     1
+    1 1
+   1 2 1
+  1 3 3 1
+ 1 4 6 4 1
+
+Observe how the nunmbers in the triangle are calculated. 
+Do it in two steps. Work on the generating the numbers first in right angle triangle
+1
+1 1
+1 2 1
+1 3 3 1
+1 4 6 4 1
+
+And then work on the final output using proper indendation
+'''
