e688501ca447bb07c64da49dc16552cbdd7166c5
Pavithra M <134091742+Pavit-9052@users.noreply.github.com>
2023-12-02 19:46:27 +0530

    Update problem2.py

--- Homeworks/Day7/problem2.py
+++ Homeworks/Day7/problem2.py
@@ -5,8 +5,32 @@ In the input, find the first A and last A, print all the letters between these t
 '''
 
 
+def find_between_letters(sentence, letter):
+    output = ''
+    first_letter_index = sentence.find(letter)
+    last_letter_index = sentence.rfind(letter)
+
+    if first_letter_index != -1 and last_letter_index != -1 :
+        for i in range(first_letter_index + 1, last_letter_index):
+            output += sentence[i]
+        print(output)
+    else:
+        print(f"No {letter} found or only one {letter} found.")
+
+def find_between_a_b(sentence):
+    find_between_letters(sentence, 'a')
+    find_between_letters(sentence, 'b')
+
+sentence = input("Enter a sentence: ")
+find_between_a_b(sentence)
+
+
 
 '''
 OUTPUT:-
+
+Enter a sentence: abcdefghbakealkb
+bcdefghbake
+cdefghbakealk
 
 '''
