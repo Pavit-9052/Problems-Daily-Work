90671cf3b3c6fe08fa99427ba6158b2a448bfeed
Pavithra M <134091742+Pavit-9052@users.noreply.github.com>
2023-12-02 20:09:34 +0530

    Update problem3.py

--- Homeworks/Day7/problem3.py
+++ Homeworks/Day7/problem3.py
@@ -5,36 +5,41 @@ Problem 3:
   
   If there is no B or 2nd B is not found, find the first C after the first B (if there is a B) and last C and print all the letters between these two C. 
 '''
-def find_between_A_B_C(sentence):
+def find_between_a_b_c(sentence):
     output = ''
     sentence = sentence.lower()
     first_A_index = sentence.find('a')
     last_A_index = sentence.rfind('a')
+    
+    
     if first_A_index == -1 or first_A_index == last_A_index:
-        if first_A_index != -1:
-            first_B_index = sentence.find('b', first_A_index)
-            last_B_index = sentence.rfind('b')
-            if first_B_index == -1 or first_B_index == last_B_index:
-                if first_B_index != -1:
-                    first_C_index = sentence.find('c', first_B_index)
-                    last_C_index = sentence.rfind('c')
-                    if first_C_index == -1 or first_C_index == last_C_index:
-                        print("No occurrences of B or C found after the first A.")
-                    else:
-                        output = sentence[first_C_index + 1:last_C_index]
-                        print(output)
-                else:
-                    print("No occurrences of B found after the first A.")
+     
+        first_B_index = sentence.find('b', first_A_index)
+        last_B_index = sentence.rfind('b')
+
+
+        if first_B_index == -1 or first_B_index == last_B_index:
+           
+            first_C_index = sentence.find('c', first_B_index)
+            last_C_index = sentence.rfind('c')
+
+            
+            if first_C_index == -1 or first_C_index == last_C_index:
+                print("No occurrences of B or C found after the first A.")
             else:
-                output = sentence[first_B_index + 1:last_B_index]
+                output = sentence[first_C_index + 1:last_C_index]
                 print(output)
         else:
-            print("No occurrences of A found in the sentence.")
+            output = sentence[first_B_index + 1:last_B_index]
+            print(output)
     else:
+       
         output = sentence[first_A_index + 1:last_A_index]
         print(output)
+
 sentence = input("Enter a sentence: ")
-find_between_A_B_C(sentence)
+find_between_a_b_c(sentence)
+
 
 
 '''
