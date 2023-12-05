c99129b513bc807a310f22cc64674a5ca99614b2
Pavithra M <134091742+Pavit-9052@users.noreply.github.com>
2023-12-05 23:48:06 +0530

    Create problem2.py

---
+++ Homeworks/Day6/problem2.py
@@ -0,0 +1,13 @@
+'''
+Program 2
+Given a collection of two numbers that specify an interval, merge all overlapping intervals. 
+For example, 
+Input [[1,3],[2,6],[8,10],[15,20],[16,25] ]
+Output [[1,6],[8,10],[15,25]].
+'''
+
+
+'''
+OUTPUT:-
+
+'''
