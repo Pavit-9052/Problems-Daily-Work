4a04890b71cd3bafc1cfcd9bcfc8c173c6fdd2be
Pavithra M <134091742+Pavit-9052@users.noreply.github.com>
2023-12-05 22:37:44 +0530

    Update problem3.py

--- Homeworks/Day2/problem3.py
+++ Homeworks/Day2/problem3.py
@@ -9,7 +9,7 @@ dice.If the rolled number is 0, game ends. If the rolled number is even, then 2 
 import random
 
 def dice_roll():
-    return random.randint(1, 6)
+    return random.randint(0, 6)
 points = 0
 while points <= 50:
     number = dice_roll()
@@ -32,36 +32,14 @@ if points >= 50:
 '''
 OUTPUT:-
 
-You rolled 5. You gained 3 points. Total points: 3
-You rolled 2. You gained 2 points. Total points: 5
-You rolled 2. You gained 2 points. Total points: 7
-You rolled 2. You gained 2 points. Total points: 9
-You rolled 2. You gained 2 points. Total points: 11
-You rolled 6. You gained 2 points. Total points: 13
-You rolled 1. You have to jump!
-You rolled 5. You gained 3 points. Total points: 16
-You rolled 5. You gained 3 points. Total points: 19
+You rolled 2. You gained 2 points. Total points: 2
+You rolled 2. You gained 2 points. Total points: 4
 You rolled 3. You have to jump!
-You rolled 6. You gained 2 points. Total points: 21
-You rolled 5. You gained 3 points. Total points: 24
+You rolled 6. You gained 2 points. Total points: 6
+You rolled 4. You gained 2 points. Total points: 8
+You rolled 4. You gained 2 points. Total points: 10
+You rolled 6. You gained 2 points. Total points: 12
+You rolled 5. You gained 3 points. Total points: 15
 You rolled 1. You have to jump!
-You rolled 6. You gained 2 points. Total points: 26
-You rolled 6. You gained 2 points. Total points: 28
-You rolled 5. You gained 3 points. Total points: 31
-You rolled 2. You gained 2 points. Total points: 33
-You rolled 4. You gained 2 points. Total points: 35
-You rolled 4. You gained 2 points. Total points: 37
-You rolled 1. You have to jump!
-You rolled 6. You gained 2 points. Total points: 39
-You rolled 6. You gained 2 points. Total points: 41
-You rolled 6. You gained 2 points. Total points: 43
-You rolled 3. You have to jump!
-You rolled 1. You have to jump!
-You rolled 2. You gained 2 points. Total points: 45
-You rolled 2. You gained 2 points. Total points: 47
-You rolled 3. You have to jump!
-You rolled 6. You gained 2 points. Total points: 49
-You rolled 3. You have to jump!
-You rolled 2. You gained 2 points. Total points: 51
-Congratulations! You won the game.
+Game Over! You rolled a 0.
 '''
