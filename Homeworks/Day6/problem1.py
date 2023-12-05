04c209df53d9444304de1ee19eb4031b250bf7d1
Pavithra M <134091742+Pavit-9052@users.noreply.github.com>
2023-12-05 23:47:27 +0530

    Create problem1.py

---
+++ Homeworks/Day6/problem1.py
@@ -0,0 +1,17 @@
+'''
+Program #1
+Write a program to check if a given string of parentheses, brackets, and braces is balanced. 
+For instance, "({[]})" is balanced, but "({[)})" is not
+Input - (Abc[i]) or (Abc[2])
+Output true
+Input ((Abc[i]) or (Abc[2])) 
+Output true
+Input ((Abc[i]) or Abc[2)])
+Output False
+'''
+
+
+'''
+OUTPUT:-
+
+'''
