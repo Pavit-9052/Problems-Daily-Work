15b4c545454dffb6075eaa3e399c80b5fe4290ea
Pavithra M <134091742+Pavit-9052@users.noreply.github.com>
2023-12-05 22:29:46 +0530

    Update problem3.py

--- Homeworks/Day1/problem3.py
+++ Homeworks/Day1/problem3.py
@@ -11,14 +11,22 @@ Try to use only one if statement.
 '''
 
 
+PASS_MARK = 50
 
-names=["Pavithra","Abisha","Sanjana","Karthic Selvi","Abirami","DhanuSri"]
-cs_marks=[78,99,56,88,60,40]
-math_marks=[32,98,45,89,70,86]
-eng_marks=[65,99,76,81,74,3]
-for n,cs,mat,eng in zip(names,cs_marks,math_marks,eng_marks):
-  if ((cs>=90 and mat>=90 and eng>=90)or cs>=80 or mat>=80 or eng>=80):
-    print(n)
+names = ["Pavithra", "Abisha", "Sanjana", "Karthic Selvi", "Abirami", "DhanuSri"]
+cs_marks = [78, 99, 56, 88, 60, 40]
+math_marks = [32, 98, 45, 89, 70, 86]
+eng_marks = [65, 99, 76, 81, 74, 3]
+
+print("Students who got A in all subjects and at least one A and B grade:")
+for n, cs, mat, eng in zip(names, cs_marks, math_marks, eng_marks):
+    if (
+        (cs >= 90 and mat >= 90 and eng >= 90) or #checking for A in all subjects
+        cs >= 90 or mat >= 90 or eng >= 90 or     #checking for A in atleast one subject
+        (cs >= 80 and mat >= 80 and eng >= 80)or  #checking for B in all subjects
+        cs >= 80 or mat >= 80 or eng >= 80        #checking for B in atleast one subject
+    ):
+        print(n)
 
 
 '''
