567484ae4fe066193371d4b2151959c4fb14fd9a
Pavithra M <134091742+Pavit-9052@users.noreply.github.com>
2023-12-09 20:23:33 +0530

    Update problem4.py

--- Homeworks/Day10/problem4.py
+++ Homeworks/Day10/problem4.py
@@ -14,7 +14,46 @@ operator, pop two elements from the stack, apply the operation and push the resu
 '''
 
 
+def calculate(expression):
+    stack = []
+    operators = ['+', '-', '*', '/']
+
+    for item in expression:
+        if item not in operators:
+            stack.append(int(item))
+        else:
+            operand_2 = stack.pop()
+            operand_1 = stack.pop()
+
+            if item == '+':
+                result = operand_1 + operand_2
+            elif item == '/':
+                result = operand_1 / operand_2
+            elif item == '-':
+                result = operand_1 - operand_2
+            else:
+                result = operand_1 * operand_2
+
+            stack.append(result)
+
+    return stack[0]
+
+
+expression1 = ["1", "2", "+", "5", "*"]
+result1 = calculate(expression1)
+print("Input 1:",expression1)
+print("Output:",result1) 
+
+expression2 = ["10", "2", "3", "+","-", "5", "*"]
+result2 = calculate(expression2)
+print("Input 2:",expression2)
+print("Output:",result2) 
+
+
 '''
 OUTPUT:-
-
+Input 1: ['1', '2', '+', '5', '*']
+Output: 15
+Input 2: ['10', '2', '3', '+', '-', '5', '*']
+Output: 25
 '''
