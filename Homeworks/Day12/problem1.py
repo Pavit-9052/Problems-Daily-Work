746fd76039c9f3b148ebcc9a6646e6b0e1eff289
Pavithra M <134091742+Pavit-9052@users.noreply.github.com>
2023-12-18 11:23:04 +0530

    Create problem1.py

---
+++ Homeworks/Day12/problem1.py
@@ -0,0 +1,13 @@
+'''
+Problem #1
+You have list of unique words. you input is a string with no spaces. Return true if the letters in the 
+input string can be seperated into words that are in the list.
+eg list = ["we", "students", "are" ]
+input = "wearestudents"
+output = true
+eg 2 List = ["we", "doctor","and", "nurse", "are"]
+input = "wearenurseandoctor"
+output = False 
+input = "wearenurseanddoctor"
+output = true
+'''
