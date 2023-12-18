1750e606baa152faffec01afb2ccb0445b382baf
Pavithra M <134091742+Pavit-9052@users.noreply.github.com>
2023-12-18 11:52:03 +0530

    Create problem3.py

---
+++ Homeworks/Day11/problem3.py
@@ -0,0 +1,9 @@
+'''
+Print all possible ipaddress based on the contraints learnt in problem 1.
+eg input - 0000
+output 0.0.0.0
+input 10026710
+output 100.2.67.10
+input 100242200
+output 100.24.220.0, 100.242.20.0
+'''
