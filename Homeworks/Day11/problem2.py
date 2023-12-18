0428a50600086381cbf066ffbe6f0714a34e21e6
Pavithra M <134091742+Pavit-9052@users.noreply.github.com>
2023-12-18 11:51:05 +0530

    Create problem2.py

---
+++ Homeworks/Day11/problem2.py
@@ -0,0 +1,12 @@
+'''
+Problem #1
+1. Check whether the given string input is a valid ip address.
+Find out what are the constraints for an ip address (how many chars, what numbers are allowed, how many '.'
+etc)
+Write all constraints.
+Get an input as string. Return if it is valid or not. 
+Use string functions.
+
+2. Same as above. Use regex.
+'''
+
