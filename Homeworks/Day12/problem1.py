6878e9a65b633b45d0ad834e14dbe5e5b1e18f0f
Pavithra M <134091742+Pavit-9052@users.noreply.github.com>
2023-12-18 11:37:27 +0530

    Update problem1.py

--- Homeworks/Day12/problem1.py
+++ Homeworks/Day12/problem1.py
@@ -11,3 +11,50 @@ output = False
 input = "wearenurseanddoctor"
 output = true
 '''
+
+
+def is_same(list_input):
+    dictionary={}
+    for item in list_input:  
+        if item in dictionary:  #If value present in dictionary increase count
+            dictionary[item]+=1
+        else:                  #If not include it in dictionary
+            dictionary[item]=1
+    return dictionary
+        
+        
+input1=input("Enter the words separated by spaces:").split()
+input_list=''.join(input1)
+
+sentence=input("Enter the sentence:")
+
+dict1=is_same(input_list)
+dict2=is_same(sentence)
+
+#To sort the dictionary for checking for equal characters
+sorted_dict1 = dict(sorted(dict1.items()))  
+sorted_dict2 = dict(sorted(dict2.items()))
+
+print(sorted_dict1)
+print(sorted_dict2)
+
+result= sorted_dict1==sorted_dict2
+print("The output:",result)
+
+'''
+OUTPUT:-
+
+Enter the words separated by spaces:we are doctor and nurse
+Enter the sentence:weandoctornurseare
+{'a': 2, 'c': 1, 'd': 2, 'e': 3, 'n': 2, 'o': 2, 'r': 3, 's': 1, 't': 1, 'u': 1, 'w': 1}
+{'a': 2, 'c': 1, 'd': 1, 'e': 3, 'n': 2, 'o': 2, 'r': 3, 's': 1, 't': 1, 'u': 1, 'w': 1}
+The output: False
+
+
+Enter the words separated by spaces:we doctor and nurse are
+Enter the sentence:wenurseanddoctorare
+{'a': 2, 'c': 1, 'd': 2, 'e': 3, 'n': 2, 'o': 2, 'r': 3, 's': 1, 't': 1, 'u': 1, 'w': 1}
+{'a': 2, 'c': 1, 'd': 2, 'e': 3, 'n': 2, 'o': 2, 'r': 3, 's': 1, 't': 1, 'u': 1, 'w': 1}
+The output: True
+
+'''
