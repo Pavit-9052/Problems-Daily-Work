d0cdd9de7e6ae6ada9b368a46351d7e640ce39f7
Pavithra M <134091742+Pavit-9052@users.noreply.github.com>
2024-01-28 17:56:19 +0530

    Create problem1.py

---
+++ Homeworks/Day9/problem1.py
@@ -0,0 +1,6 @@
+'''
+1. **Weather Data Retrieval:**
+   Write a program that fetches the current weather data for a specific city using a weather API.
+   - Utilize a weather API (such as OpenWeatherMap) to retrieve the current weather details (temperature, humidity, wind speed, etc.) for a user-input city.
+   '''
+
